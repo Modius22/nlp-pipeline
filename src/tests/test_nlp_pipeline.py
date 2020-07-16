5fba631d5c3230108ad5e4d82d428e403c94831a
Modius22 <christian.piazzi@gmx.de>
2020-07-16 12:33:14 +0200

    Update test_nlp_pipeline.py
    testing

--- src/tests/test_nlp_pipeline.py
+++ src/tests/test_nlp_pipeline.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import pandas as pd
+
 from api import app
 
 class NlpPipeline(unittest.TestCase):
@@ -8,18 +10,40 @@ class NlpPipeline(unittest.TestCase):
     self.app = app.test_client()
 
   def test_complete_pipeline(self):
- #   # create directory
- #   assert os.path.exists('../working') == 1
- #   response = self.app.get('/nlp/create_project?project_name=project_a')
- #   self.assertEqual(200, response.status_code)
- #   assert os.path.exists('../working/project_a') == 1
-    print(os.getcwd())
+#    # create directory
+#    assert os.path.exists('../working') == 1
+#    response = self.app.get('/nlp/create_project?project_name=project_a')
+#    self.assertEqual(200, response.status_code)
+#    assert os.path.exists('../working/project_a') == 1
+#    print(os.getcwd())
+
+#    res = self.client.post('/', data=dict(
+#      upload_var=(StringIO("hi everyone"), 'test.txt'),
+#    ))
+#    assert res.status_code == 200
+#    assert 'file saved' in res.data
+
 
     # Load Data
     assert os.path.exists('../working/project_a') == 1
-    response = self.app.post('/nlp/project_a/load?text=text&sentiment=airline_sentiment', headers={'enctype': 'multipart/form-data', 'Content-Type': 'multipart/form-data'}, data='../data/Tweets.csv')
+
+
+    df = pd.read_csv('../data/Tweets.csv')
+    csv = df.to_csv(index=False)
+    response = self.app.post('/nlp/project_a/load?text=text&sentiment=airline_sentiment', headers={'enctype': 'multipart/form-data', 'Content-Type': 'multipart/form-data'}, data=csv)
     self.assertEqual(200, response.status_code)
     assert os.path.isfile('../working/project_a/project_a_raw.feather') == 1
+
+
+#    # Load Data
+#    assert os.path.exists('../working/project_a') == 1
+#    response = self.app.post('/nlp/project_a/load?text=text&sentiment=airline_sentiment', headers={'enctype': 'multipart/form-data', 'Content-Type': 'multipart/form-data'}, data='../data/Tweets.csv')
+#    self.assertEqual(200, response.status_code)
+#    assert os.path.isfile('../working/project_a/project_a_raw.feather') == 1
+
+
+
+
 
     # Explore data
     assert os.path.exists('../working/project_a') == 1
